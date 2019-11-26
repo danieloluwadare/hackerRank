32b50ba379162bb2c0d8b3894668186dac0fb18c
Daniel Dada <ddada@teamapt.com>
2019-11-26 13:24:30 +0100

    Array manipulation question solution 3 optimal solution

--- src/com/company/ArrayManipulationHrk.java
+++ src/com/company/ArrayManipulationHrk.java
@@ -29,50 +29,64 @@ public class ArrayManipulationHrk {
     }
 
     static long arrayManipulation(int n, int[][] queries) {
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-        boolean check=true;
         long maxNumber = Integer.MIN_VALUE;
-        int sum = 0;
+        long sum = 0;
+        int [] resultant = new int[n + 2];
         for(int i = 0; i<queries.length; i++){
-            for(int j =0; j<2; j++){
-                if(check==true){
-                    if(map.containsKey(queries[i][j])){
-                        int newValue = map.get(queries[i][j]) + queries[i][2];
-                        map.replace(queries[i][j],newValue);
-
-                    }else {
-                        map.put(queries[i][j],queries[i][2]);
-
-
-                    }
-                    check=!check;
-                    continue;
-                }
-                else {
-                    if(map.containsKey(queries[i][j])){
-                        int newValue = map.get(queries[i][j]) + -queries[i][2];
-                        map.replace(queries[i][j],newValue);
-
-                    }else{
-                        map.put(queries[i][j],-queries[i][2]);
-
-                    }
-                    check=!check;
-                }
-
-//                System.out.print(queries[i][j]);
-            }
-//            System.out.println();
+            resultant[queries[i][0]] += queries[i][2];
+            resultant[(queries[i][1] + 1)] -= queries[i][2];
         }
-
-        for (Object i: map.keySet()) {
-            System.out.println("key="+i+" value= "+map.get(i));
-        }
-
-        for(Integer i : map.values()){
+        for(int i : resultant){
             sum +=i;
             maxNumber=Math.max(maxNumber,sum);
         }
+
+
+//        Map<Integer, Integer> map = new LinkedHashMap<>();
+//        boolean check=true;
+//        long maxNumber = Integer.MIN_VALUE;
+//        int sum = 0;
+
+//        for(int i = 0; i<queries.length; i++){
+//            for(int j =0; j<2; j++){
+//                if(check==true){
+//                    if(map.containsKey(queries[i][j])){
+//                        int newValue = map.get(queries[i][j]) + queries[i][2];
+//                        map.replace(queries[i][j],newValue);
+//
+//                    }else {
+//                        map.put(queries[i][j],queries[i][2]);
+//
+//
+//                    }
+//                    check=!check;
+//                    continue;
+//                }
+//                else {
+//                    if(map.containsKey(queries[i][j])){
+//                        int newValue = map.get(queries[i][j]) + -queries[i][2];
+//                        map.replace(queries[i][j],newValue);
+//
+//                    }else{
+//                        map.put(queries[i][j],-queries[i][2]);
+//
+//                    }
+//                    check=!check;
+//                }
+//
+////                System.out.print(queries[i][j]);
+//            }
+////            System.out.println();
+//        }
+
+//        for (Object i: map.keySet()) {
+//            System.out.println("key="+i+" value= "+map.get(i));
+//        }
+//
+//        for(Integer i : map.values()){
+//            sum +=i;
+//            maxNumber=Math.max(maxNumber,sum);
+//        }
 
 
 
